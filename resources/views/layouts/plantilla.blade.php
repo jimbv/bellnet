1b57215535cfd16a8b4711b00ceb3d456ec375c4
Jose Martin <joseignaciomartin@gmail.com>
2025-10-15 15:17:21 -0300

    mejoras

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -206,9 +206,9 @@
                     </div>
                 </div>
                 <div class=" text-sm flex flex-col sm:flex-row justify-center items-center gap-5 pb-10" style="color: white;">
-                    <a href="/nuestra-empresa" style="color: white;text-decoration: none;">Nuestra empresa</a>
+                    <a href="/page/nuestra-empresa" style="color: white;text-decoration: none;">Nuestra empresa</a>
                     <a href="/novedades" style="color: white;text-decoration: none;">Noticias</a>
-                    <a href="/preguntas-frecuentes" style="color: white;text-decoration: none;">Preguntas frecuentes</a>
+                    <a href="/page/preguntas-frecuentes" style="color: white;text-decoration: none;">Preguntas frecuentes</a>
                     <a target="_blank" href="https://www.instagram.com/clickcomunicacion.vm/" style="color: white;text-decoration: none;">
                         <i class="fab fa-instagram"></i> clickcomunicacion.vm
                     </a>
