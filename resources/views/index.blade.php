56407ea62a584a0ccb362b3132616f1737775d13
Jose Martin <joseignaciomartin@gmail.com>
2025-11-03 11:39:24 -0300

    mejora en texto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,9 +34,8 @@
     <div class="container py-3" style="position: relative; top: -70px;z-index:9;background:#f74e05;color:white;">
         <div class="row justify-content-center">
             <div class="col-12 col-md-10 col-lg-11 text-center" style="font-size:18px;font-weight:bold;letter: spacing 1px;">
-                Ayudamos a potenciar tu marca, trabajando las herramientas de comunicación adecuadas y posicionándola a través de distintos espacios de publicidad y difusión.
-
-                Contamos con un amplio abanico de medios en todos los formatos publicitarios y producimos campañas efectivas para llevar estratégicamente tu marca a todas partes y a otro nivel.
+                Ayudamos a potenciar marcas, posicionándolas a través de distintos espacios de comunicación, publicidad y difusión. 
+                Producimos campañas efectivas a través de un amplio abanico de medios y formatos, que nos permiten llevar estratégicamente marcas a todas partes y a otro nivel.
                 </p>
             </div>
         </div>
